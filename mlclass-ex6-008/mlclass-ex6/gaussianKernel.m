0ba40223bfb886b88423b4fdaa8cfefc4c6f8808
kurtpayne <kpayne@gmail.com>
2015-03-14 10:26:37 -0700

    Solved

--- mlclass-ex6-008/mlclass-ex6/gaussianKernel.m
+++ mlclass-ex6-008/mlclass-ex6/gaussianKernel.m
@@ -6,8 +6,10 @@ function sim = gaussianKernel(x1, x2, sigma)
 % Ensure that x1 and x2 are column vectors
 x1 = x1(:); x2 = x2(:);
 
+sim = exp((- sum((x1 - x2) .^ 2)) / 2 / sigma ^ 2);
+
 % You need to return the following variables correctly.
-sim = 0;
+% sim = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
